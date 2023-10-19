04f3f5e898f7f559d789f87a91a8b51ad81f4f50
@brillianita <brillianitaaa@gmail.com>
2023-10-19 11:01:37 +0800

    feat: adding addUser service in UsersService.js

---
+++ src/services/UsersService.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../exceptions/InvariantError');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({
+    fullname,
+    username,
+    password,
+    phoneNumber,
+    address,
+    gender,
+  }) {
+    await this.verifyNewUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, fullname, username, hashedPassword, phoneNumber, address, gender],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User failed to add');
+    }
+    return result.rows[0].id;
+  }
+
+  async verifyNewUser(username, phoneNumber) {
+    const query = {
+      text: 'SELECT username, phone_number FROM users WHERE username = $1 OR phone_number = $2',
+      values: [username, phoneNumber],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('User failed to add. Username or phone number have been used');
+    }
+  }
+}
+
+module.exports = UsersService;
