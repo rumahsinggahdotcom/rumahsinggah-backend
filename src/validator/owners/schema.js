51a43c7fbc7cb2dc6b18a9d290eca736f7f3c598
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-10-24 13:31:48 +0800

    feat: adding validation for username,password,phoneNumber,newPassword

--- src/validator/owners/schema.js
+++ src/validator/owners/schema.js
@@ -2,16 +2,25 @@ const Joi = require('joi');
 
 const OwnersPayloadSchema = Joi.object({
   fullname: Joi.string().required(),
-  username: Joi.string().required(),
-  password: Joi.string().required(),
+  username: Joi.string().trim().required(),
+  password: Joi.string().regex(/(?=.*[a-z])[A-Za-z\d$@$!%*?&.]{8,20}/)
+    .required()
+    .min(8)
+    .max(20),
   address: Joi.string().required(),
   gender: Joi.string().valid('Laki-Laki', 'Perempuan'),
-  phoneNumber: Joi.string().required(),
+  phoneNumber: Joi.string().pattern(/^\d+$/)
+    .min(8)
+    .max(13)
+    .required(),
 });
 
 const OwnersPasswordPayloadSchema = Joi.object({
   oldPassword: Joi.string().required(),
-  newPassword: Joi.string().required(),
+  newPassword: Joi.string().regex(/(?=.*[a-z])[A-Za-z\d$@$!%*?&.]{8,20}/)
+    .required()
+    .min(8)
+    .max(20),
 });
 
 module.exports = { OwnersPayloadSchema, OwnersPasswordPayloadSchema };
