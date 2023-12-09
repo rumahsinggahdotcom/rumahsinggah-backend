ab018fac99ade9a08b0764e0d1b02ff66a0dbf45
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-09 22:35:18 +0800

    feat: changing totalprice validator

--- src/validator/bookings/schema.js
+++ src/validator/bookings/schema.js
@@ -4,7 +4,7 @@ const BookingsPayloadSchema = Joi.object({
   start: Joi.date().required(),
   end: Joi.date().required(),
   status: Joi.string().required(),
-  totalPrice: Joi.integer().required(),
+  totalPrice: Joi.number().integer().options({ convert: false }).required(),
 });
 
 module.exports = { BookingsPayloadSchema };
