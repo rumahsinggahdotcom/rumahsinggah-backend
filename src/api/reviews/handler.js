fbb2ca1f33ebfa4c4c8202bc7d1207c44ee128ab
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-19 22:11:47 +0800

    feat: add post review handler

---
+++ src/api/reviews/handler.js
@@ -0,0 +1,46 @@
+const autoBind = require('auto-bind');
+
+class ReviewsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postReviewsHandler(request, h) {
+    const {
+      userId,
+      kosId,
+      score,
+      review,
+    } = request.payload;
+
+    await this._validator.validateReviewsPayload({
+      userId,
+      kosId,
+      score,
+      review,
+    });
+
+    const id = await this._service.addReview({
+      userId,
+      kosId,
+      score,
+      review,
+    });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Review berhasil ditambahkan',
+      data: {
+        id,
+      },
+    });
+
+    response.code(200);
+    return response;
+  }
+}
+
+module.exports = ReviewsHandler;
