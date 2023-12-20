c2f5f20513c438ac28caeba242e6645e4bb2dcb0
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-20 13:35:09 +0800

    deleting roomnumvalidator in koss validator

--- src/validator/koss/schema.js
+++ src/validator/koss/schema.js
@@ -11,11 +11,6 @@ const KosImagePayloadSchema = Joi.object({
   'content-type': Joi.string().valid('image/apng', 'image/afiv', 'image/gif', 'image/jpeg', 'image/png', 'image/webp'),
 }).unknown();
 
-const RoomNumSchema = Joi.object({
-  number: Joi.number().integer().options({ convert: false }).required(),
-  status: Joi.string().required(),
-});
-
 module.exports = {
-  KosPayloadSchema, KosImagePayloadSchema, RoomNumSchema,
+  KosPayloadSchema, KosImagePayloadSchema,
 };
