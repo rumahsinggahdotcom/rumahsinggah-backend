06a17f67dbf51d0b5463394f018a60910be404d8
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-20 13:36:23 +0800

    feat: add edit review validator

--- src/validator/reviews/index.js
+++ src/validator/reviews/index.js
@@ -1,11 +1,18 @@
-const { RoomsPayloadSchema } = require('./schema');
+const { ReviewPayloadSchema, EditReviewSchemaPayload } = require('./schema');
 const InvariantError = require('../../exceptions/InvariantError');
 
 const ReviewsValidator = {
   validateReviewPayload: (payload) => {
-    const reviewValidationResult = RoomsPayloadSchema.validate(payload);
+    const reviewValidationResult = ReviewPayloadSchema.validate(payload);
     if (!reviewValidationResult) {
       throw new InvariantError(reviewValidationResult.error.message);
+    }
+  },
+
+  validateEditReviewPayload: (payload) => {
+    const editReviewValidationResult = EditReviewSchemaPayload.validate(payload);
+    if (!editReviewValidationResult) {
+      throw new InvariantError(editReviewValidationResult.error.message);
     }
   },
 };
