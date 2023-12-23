6fc998e95766b83ab51e76ae0d9d7c3180fcd85d
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2023-12-23 19:15:36 +0800

    db: remove rating attribute from koss table

--- migrations/1697681111311_create-table-koss.js
+++ migrations/1697681111311_create-table-koss.js
@@ -24,16 +24,10 @@ exports.up = (pgm) => {
       type: 'VARCHAR',
       notNull: true,
     },
-    rating: {
-      type: 'FLOAT',
-      notNull: false,
-    },
   });
   pgm.addConstraint('koss', 'fk_koss.owner_id_owners.id', 'FOREIGN KEY(owner_id) REFERENCES owners(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
-  // pgm.dropConstraint('koss', 'fk_koss_pemilik_id_pemilik_koss.id');
-  // pgm.dropConstraint('koss', 'fk_koss_users_id_users.id');
   pgm.dropTable('koss');
 };
