7e4e8116e83b80ba3b0d7d1809eefcc325245bf6
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-05-20 21:59:15 +0800

    feat: add get access token exp function

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -4,6 +4,13 @@ const InvariantError = require('../exceptions/InvariantError');
 const TokenManager = {
   generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
   generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+  getAccessTokenExp: (accessToken) => {
+    jwtDecoded = Jwt.token.decode(accessToken)
+    // console.log("jwtDecoded", jwtDecoded)
+    accessTokenIat = jwtDecoded.decoded.payload.iat
+    accessTokenExp = accessTokenIat + parseInt(process.env.ACCESS_TOKEN_AGE)
+    return { accessTokenExp, accessTokenIat }
+  },
   verifyRefreshToken: (refreshToken) => {
     try {
       const artifacts = Jwt.token.decode(refreshToken);
